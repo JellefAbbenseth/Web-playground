6833c9e2aea313d7435fa87de309a37a4bdba8b6
Jellef Abbenseth <jellef@gmx.de>
2022-07-16 15:38:16 +0200

    Playground
    - under-construction site

---
+++ Playground/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  alttext: string = "under-construction";
+  path: string = "../assets/under-construction.png";
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Web-playground
+## UNDER CONSTRUCTION
+
+First try angular to gh-pages!
+
 A web page as a playground for different websites.
