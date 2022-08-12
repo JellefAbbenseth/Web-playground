78a828c904a82f12f1dd0a49ea3c730b6c6e3424
Jellef Abbenseth <jellef@gmx.de>
2022-08-12 11:56:33 +0200

    Playground
    - finished Quiz

--- src/app/websites/quiz/quiz.component.ts
+++ src/app/websites/quiz/quiz.component.ts
@@ -1,6 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { delay } from 'rxjs';
 
 const colorGreen: string = "#59CE8F";
 const  colorRed: string = "#FF1E00";
@@ -12,16 +12,61 @@ const  colorGrey: string = "#E8F9FD";
   styleUrls: ['./quiz.component.scss']
 })
 export class QuizComponent implements OnInit {
-  unanswered = true
   quiz = new FormGroup({
     answer: new FormControl("", [Validators.required]),
   });
-  text: string = "In tennis, what piece of fruit is found at the top of the men's Wimbledon trophy?";
-  right_answer = "Pineapple";
+  
+  unanswered = true
+  quiz_ends = false;
+  number_of_questions = 2;
 
-  constructor() { }
+  question_text: string = "";
+  right_answer: string = "";
+  question_index: number = 0;
+  answeredCorrect: number = 0; 
+
+  questions_data: any = [];
+  questions_list: string[] = [];
+
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit(): void {
+    this.httpClient.get("../../../assets/quiz/questions.json").subscribe(data => {
+      this.questions_data = data;
+      this.startQuiz()
+    })
+  }
+
+  startQuiz() {
+    this.questions_list = []
+    this.answeredCorrect = 0
+    this.question_index = 0
+    this.unanswered = true
+    this.quiz_ends = false
+    this.chooseQuestions()
+    this.setNextQuestion()
+    this.quiz.reset()
+  }
+
+  chooseQuestions() {
+    var index_list: number[] = []
+    var num = 0
+    while (true) {
+      num = Math.floor(Math.random() * this.questions_data.length)
+      if (index_list.includes(num)) continue
+      else index_list.push(num)
+      
+      if (index_list.length >= this.number_of_questions) break
+    }
+
+    for (var i = 0; i < index_list.length; i++) {
+      this.questions_list.push(this.questions_data[index_list[i]])
+    }
+  }
+
+  setNextQuestion() {
+    this.question_text = this.questions_list[this.question_index][0]
+    this.right_answer = this.questions_list[this.question_index][1]
   }
 
   onSubmit() {
@@ -32,21 +77,35 @@ export class QuizComponent implements OnInit {
     var element = document.getElementById("card")
     if (element != null) {
       if (user_answer  == this.right_answer.toLowerCase()) {
+        this.answeredCorrect++
         element.style.backgroundColor = colorGreen
       } else {
         element.style.backgroundColor = colorRed
       }
     } 
       
-
     this.quiz.reset()
   }
 
   onNext() {
-    this.unanswered = true
     var element = document.getElementById("card")
     if (element != null) element.style.backgroundColor = colorGrey
+    
+    this.question_index++
+    if (this.question_index < this.questions_list.length) {
+      this.unanswered = true
+      this.setNextQuestion()
+    } else {
+      this.quiz_ends = true
+      this.endQuiz()
+    }
   }
 
+  endQuiz() {
+    this.question_text = "Well done! You have " + this.answeredCorrect + " from " + this.number_of_questions +" correct! Following is a list of the right answers: "
+    for (var i = 0; i < this.questions_list.length; i++) {
+      this.question_text += (i + 1) +". " +this.questions_list[i][1] +", "
+    }
+  }
 }
 
